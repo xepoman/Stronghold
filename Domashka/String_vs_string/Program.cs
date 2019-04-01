77ef9273f27e0e5ee45ddd1d8ae8d2f84d72938a
xepoman <ivan.egov@mail.ru>
2019-04-01 10:29:37 +0700

    Исправил ошибку

---
+++ Domashka/String_vs_string/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**даны две строки
+ * в каждую строку водит пользователь
+ *ишем в первой строке  кол-во второй 
+ * ссчитаем какой номер индекса при старте
+ */
+namespace String_vs_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую строку: ");
+            string str1 = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            string str2 = Console.ReadLine();
+            int count = 0;
+            string temp_str = "";
+            for (int i =0; i<str1.Length; i++)
+            {
+               
+                int start = i;
+                int end = i + str2.Length - 1;
+                
+                for(int t = start; t <= end; t++)
+                {
+                   if(end!=str1.Length) temp_str += str1[t];
+
+                    if (temp_str == str2)
+                    {
+                        count++; // кол-во повторений строки в строке
+                         
+                    }
+                    
+                }
+                temp_str = "";
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Domashka/String_vs_string/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("String_vs_string")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("String_vs_string")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d31ce18b-8156-4b89-a17d-c4515a0be3b0")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
