fbf488c464e4b91192713fc85cf87c265c409ac2
xepoman <ivan.egov@mail.ru>
2019-04-05 12:10:17 +0700

    Создание Лабиринта

---
+++ Domashka/String_vs_string/Constructor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_vs_string
+{
+    class Constructor
+    {
+        private GeneratorDataMaze dataGenerator;
+
+        public int[,] data
+        {
+            get; private set;
+        }
+
+        //1- означает стену, а 0 — пустое пространство, то есть сетка по умолчанию выглядит как окружённая стеной комната.
+        void Awake()
+        {
+            dataGenerator = new GeneratorDataMaze();
+         //--------------------------------------------------------   
+            data = new int[,]       // для теста
+            {                       //
+            {1, 1, 1},              // =========
+            {1, 0, 1},              // ===...===
+            {1, 1, 1}               // =========
+            };
+         //---------------------------------------------------------
+            GenerateNewMaze(13, 15); // вызвали размер сетки
+        }
+
+         public void Display()
+        {
+            Awake();
+            int[,] maze = data;
+            int rMax = maze.GetUpperBound(0);
+            int cMax = maze.GetUpperBound(1);
+
+            string msg = "";
+
+            //Заменим цифры масива
+            for (int i = rMax; i >= 0; i--)
+            {
+                for (int j = 0; j <= cMax; j++)
+                {
+                    if (maze[i, j] == 0)
+                    {
+                        msg += "...";
+                    }
+                    else
+                    {
+                        msg += "===";
+                    }
+                }
+                msg += "\n";
+            }
+            Console.WriteLine(msg);
+        }
+
+        // создадим метод определяюший размере лабиринта (кол строк и столбцов)
+        public void GenerateNewMaze(int sizeRows, int sizeCols)
+        {
+            if (sizeRows % 2 == 0 && sizeCols % 2 == 0)
+            {
+               Console.Write("Нечетные числа работают лучше для размера подземелья.");
+            }
+
+            this.data = dataGenerator.FromDimensions(sizeRows, sizeCols);
+        }
+    }
+}
--- Domashka/String_vs_string/Program.cs
+++ Domashka/String_vs_string/Program.cs
@@ -14,32 +14,10 @@ namespace String_vs_string
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите первую строку: ");
-            string str1 = Console.ReadLine();
-            Console.Write("Введите вторую строку: ");
-            string str2 = Console.ReadLine();
-            int count = 0;
-            string temp_str = "";
-            for (int i =0; i<str1.Length; i++)
-            {
-               
-                int start = i;
-                int end = i + str2.Length - 1;
-                
-                for(int t = start; t <= end; t++)
-                {
-                   if(end!=str1.Length) temp_str += str1[t];
 
-                    if (temp_str == str2)
-                    {
-                        count++; // кол-во повторений строки в строке
-                         
-                    }
-                    
-                }
-                temp_str = "";
-            }
-            Console.WriteLine(count);
+            Constructor C = new Constructor();
+           
+            C.Display();
             Console.ReadKey();
         }
     }
